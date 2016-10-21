243ca25b39b4d90dec6606d61e0e7971a49cdaa1
Hafidz <hafidznrulyaqin@yahoo.com>
2016-10-21 16:48:02 +0700

    perubahan pada menu left

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -8,12 +8,15 @@
 //                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
                     // ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii']],
                     // ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug']],
-                    ['label' => 'Stock', 'icon' => 'fa fa-bar-chart', 'url' => ['/master/stock']],
-                    ['label' => 'Customer', 'icon' => 'fa fa-users', 'url' => ['/master/customer']],
+                    ['label' => 'Stock', 'icon' => 'fa fa-bar-chart', 'url' => ['/master/stock']],                    
                     ['label' => 'Tipe', 'icon' => 'fa fa-folder-o', 'url' => ['/master/tipe']],
+                    ['label' => 'Jenis', 'icon' => 'fa fa-file-o', 'url' => ['/master/jenis']],
+                    ['label' => 'Customer', 'icon' => 'fa fa-users', 'url' => ['/master/customer']],
+                    ['label' => 'Helper', 'icon' => 'fa fa-user', 'url' => ['/master/helper']],
                     ['label' => 'SO', 'icon' => 'fa fa-mail-reply', 'url' => ['/transaction/so']],
                     ['label' => 'Orderan', 'icon' => 'fa fa-pencil-square-o', 'url' => ['/transaction/orderan']],
-
+                    ['label' => 'Setoran', 'icon' => 'fa fa-money', 'url' => ['/transaction/setoran']],
+                    
                 ],
             ]
         ) ?>
