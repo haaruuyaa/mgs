f6e3e9d443261f474c220ab374cf007a64187026
Hafidz <hafidznrulyaqin@yahoo.com>
2016-11-17 22:02:09 +0700

    add master pelanggan

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -10,7 +10,7 @@
                          'items' => [
                         ['label' => 'Stock', 'icon' => 'fa fa-bar-chart', 'url' => ['/master/master-stock']],                    
                         ['label' => 'Jenis', 'icon' => 'fa fa-file-o', 'url' => ['/master/master-jenis']],
-                        ['label' => 'Customer', 'icon' => 'fa fa-users', 'url' => ['/master/master-customer']],
+                        ['label' => 'Customer', 'icon' => 'fa fa-user', 'url' => ['/master/master-customer']],
                         ['label' => 'Helper', 'icon' => 'fa fa-user', 'url' => ['/master/master-helper']],
                         ['label' => 'Harga Customer', 'icon' => 'fa fa-usd', 'url' => ['/master/harga-customer']],
                         ['label' => 'Harga Helper', 'icon' => 'fa fa-usd', 'url' => ['/master/harga-helper']],
@@ -19,6 +19,7 @@
 //                    ['label' => 'SO', 'icon' => 'fa fa-mail-reply', 'url' => ['/transaction/soh']],
 //                    ['label' => 'Pengeluaran', 'icon' => 'fa fa-money', 'url' => ['/transaction/pengeluaran-h']],
 //                    ['label' => 'Orderan', 'icon' => 'fa fa-pencil-square-o', 'url' => ['/transaction/order-h']],
+                    ['label' => 'Data Pelanggan', 'icon' => 'fa fa-users', 'url' => ['/master/master-member']],
                     ['label' => 'Setoran', 'icon' => 'fa fa-money', 'url' => ['/transaction/setoran-h']],
                     
                 ],
