97850e62efeb563c541143d03684a4fd396dc6ff
Hafidz <hafidznurulyaqin@gmail.com>
2016-12-25 22:04:26 +0700

    fix

--- master/models/StockHelperHistory.php
+++ master/models/StockHelperHistory.php
@@ -47,7 +47,7 @@ class StockHelperHistory extends \yii\db\ActiveRecord
     {
         return [
             'StockHelpId' => 'Stock Help ID',
-            'StockHelpSeqId' => 'Stock Help Seq ID', 
+            'StockHelpSeqId' => 'Stock Help Seq ID',
             'HelperId' => 'Helper ID',
             'JenisId' => 'Jenis ID',
             'Isi' => 'Isi',
@@ -55,6 +55,6 @@ class StockHelperHistory extends \yii\db\ActiveRecord
             'DateAdd' => 'Date Add',
             'DateUpdate' => 'Date Update',
             'DateCrt' => 'Date Crt',
-        ];
+        ];  
     }
 }
